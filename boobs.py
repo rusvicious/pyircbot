e02568d5524c3a421d0545572763bdf56bdcf26c
Ruslan panov <parusoft@yandex.ru>
2013-09-09 21:26:26 +0400

    Some fixes

--- boobs.py
+++ boobs.py
@@ -1,4 +1,5 @@
-import random, urllib2
+import random
+import urllib2
 
 
 class getBoobsUrl:
@@ -7,14 +8,12 @@ class getBoobsUrl:
 
     def getUrl(self):
         result = True
-
         number = random.randrange(7630)
         url = 'http://media.oboobs.ru/boobs/0%s.jpg' % (str(number))
         self.__url = url
 
         try:
             urllib2.urlopen(url)
-
         except urllib2.HTTPError, e:
             if e.code != 200:
                 result = False
