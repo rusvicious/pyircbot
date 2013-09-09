952fb66544ecef8232674f2c25203bc8951f86d4
Ruslan panov <parusoft@yandex.ru>
2013-09-09 21:56:46 +0400

    Some fixes

--- pyircbot.py
+++ pyircbot.py
@@ -2,6 +2,7 @@
 # -*- coding: cp1251 -*-
 
 import socket
+import time
 from config import botConfig
 from boobs import getBoobsUrl
 from kote import getkoteUrl
@@ -10,21 +11,26 @@ from kote import getkoteUrl
 class ircBot:
     def __init__(self):
         self.__config = botConfig()
+        self.__attemptscount = 0;
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.Connect()
+        self.try_connect()
+
+    def try_connect(self):
+        if not(self.Connect()):
+            if (self.__attemptscount <= 20):
+                time.sleep(15)
+                self.__attemptscount+= 1
+                self.try_connect()
+
 
     def Connect(self):
-        try:
-            self.sock.connect((self.__config.host, 6667))
-            self.sock.send(
-                'USER %s host servname : %s  - Python Bot by RusVicious\r\n' % (self.__config.uname, self.__config.nick))
-            self.sock.send('NICK %s\r\n' % (self.__config.nick ))
-            self.sock.send('IDENTIFY %s\r\n' % (self.__config.password))
-            self.sock.send('JOIN %s\r\n' % (self.__config.channel))
-            self.__listening()
-
-        except socket.error, e:
-            self.Connect()
+        self.sock.connect((self.__config.host, 6667))
+        self.sock.send(
+            'USER %s host servname : %s  - Python Bot by RusVicious\r\n' % (self.__config.uname, self.__config.nick))
+        self.sock.send('NICK %s\r\n' % (self.__config.nick ))
+        self.sock.send('IDENTIFY %s\r\n' % (self.__config.password))
+        self.sock.send('JOIN %s\r\n' % (self.__config.channel))
+        self.__listening()
 
     def sendMessage(self, msg):
         self.sock.send('PRIVMSG %s :%s\r\n' % (self.__config.channel, str(msg)))
@@ -58,7 +64,7 @@ class ircBot:
                 print "[GET]", self.__text
 
             except socket.error, e:
-                self.Connect()
+                self.try_connect()
 
 
 if __name__ == '__main__':
