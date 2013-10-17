d9af8ccfca8f7613f1bab9af3f25bd49b9db839c
Ruslan Panov <parusoft@yandex.ru>
2013-10-17 11:25:34 +0400

    Some fix

--- pyircbot.py
+++ pyircbot.py
@@ -14,33 +14,36 @@ from plugins.kote import getkoteUrl
 class ircBot:
     def __init__(self):
         self.__config = botConfig()
+        self.connected = False;
         self.__attemptscount = 0;
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.try_connect()
 
     def try_connect(self):
-        if not(self.Connect()):
+        if not(self.connected == True):
             if (self.__attemptscount <= 20):
-                time.sleep(15)
+                if (self.__attemptscount > 0):
+                    print "Trying to connect to", self.__config.host                  
+                    time.sleep(15);
                 self.__attemptscount+= 1
-                self.try_connect()
-
-
+                self.Connect()
+				
     def Connect(self):
         try:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.sock.connect((self.__config.host, 6667))
             self.sock.send(
                 'USER %s host servname : %s  - Python Bot by RusVicious\r\n' % (self.__config.uname, self.__config.nick))
             self.sock.send('NICK %s\r\n' % (self.__config.nick ))
             self.sock.send('IDENTIFY %s\r\n' % (self.__config.password))
             self.sock.send('JOIN %s\r\n' % (self.__config.channel))
-            result = True
+            self.connected = True
+            self.__attemptscount = 0;
             self.__listening()
 
         except socket.error,e:
-            result = False
-
-        return result
+            self.connected = False
+            print "Could not connect to", self.__config.host  
+            self.try_connect()
 
     def sendMessage(self, msg):
         self.sock.send('PRIVMSG %s :%s\r\n' % (self.__config.channel, str(msg)))
@@ -51,7 +54,7 @@ class ircBot:
         return string
 
     def __listening(self):
-        while 1:
+        while self.connected:
             try:
                 self.__text = self.sock.recv(2040)
                 if not self.__text:
@@ -71,13 +74,16 @@ class ircBot:
                 if self.__text.find(':!котэ') != -1:
                     kote = getkoteUrl()
                     self.sendMessage(kote.url)
-
+					
+                if self.__text.find(':!quit') != -1:
+                    self.sock.send('QUIT Bye!\r\n')
 
                 print "[GET]", self.__text
 
             except socket.error, e:
+                self.connected = False
+                print "Disconnected from", self.__config.host 
                 self.try_connect()
-
-
+                
 if __name__ == '__main__':
     bot = ircBot()
